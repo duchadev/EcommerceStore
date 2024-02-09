5e3ba06787c41da4b9883f9c5f038f476017520a
hannguu <95076379+hannguu@users.noreply.github.com>
2024-02-09 23:07:27 +0700

    Order tracking and view order details

--- EcommerceStore/src/main/java/com/example/EcommerceStore/repository/OrderDetailRepository.java
+++ EcommerceStore/src/main/java/com/example/EcommerceStore/repository/OrderDetailRepository.java
@@ -1,10 +1,11 @@
 package com.example.EcommerceStore.repository;
 
 import com.example.EcommerceStore.entity.OrderDetail;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderDetailRepository extends JpaRepository<OrderDetail,Integer> {
-
+public interface OrderDetailRepository extends JpaRepository<OrderDetail, Long> {
+  List<OrderDetail> findOrderDetailsByOrderOrderId(int orderId);
 }
