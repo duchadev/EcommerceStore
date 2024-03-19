87579b8939162533f65d05f484229cfc7aa8bd60
hannguu <95076379+hannguu@users.noreply.github.com>
2024-03-19 15:53:20 +0700

    update price filter

--- EcommerceStore/src/main/java/com/example/EcommerceStore/controller/ProductController.java
+++ EcommerceStore/src/main/java/com/example/EcommerceStore/controller/ProductController.java
@@ -52,11 +52,11 @@ public class ProductController {
         // Standard UserDetails case
         String email = userDetails.getUsername();
         model.addAttribute("user_email", email);
-        session.setAttribute("user_email",email);
+        session.setAttribute("user_email", email);
         User user = userRepository.findByUserEmail(email);
         model.addAttribute("userRepository", userRepository);
-        session.setAttribute("user",user);
-        System.out.println("User: "+user.getUserId());
+        session.setAttribute("user", user);
+        System.out.println("User: " + user.getUserId());
 
         int user_id = user.getUserId();
         model.addAttribute("user_id", user_id);
@@ -118,8 +118,8 @@ public class ProductController {
         model.addAttribute("user_email",
             attributes.get("email"));
         model.addAttribute("userRepository", userRepository);
-        User user = (User)session.getAttribute("user");
-        model.addAttribute("user",user);
+        User user = (User) session.getAttribute("user");
+        model.addAttribute("user", user);
 
       } else {
         return "error";
@@ -143,31 +143,31 @@ public class ProductController {
 
   @GetMapping("/productFilter/{product_type}")
   public String productFilter(@PathVariable("product_type") String product_type, Model model
-  ,HttpSession session) {
+      , HttpSession session) {
     List<Product> listProduct = productService.searchFilteredProduct(product_type);
     model.addAttribute("listProduct", listProduct);
     String email = String.valueOf(session.getAttribute("user_email"));
     model.addAttribute("user_email", email);
     model.addAttribute("productType", product_type);
-    User user = (User)session.getAttribute("user");
-    model.addAttribute("user",user);
+    User user = (User) session.getAttribute("user");
+    model.addAttribute("user", user);
 //    System.out.println("User id in product filter: "+ user.getUserId());
     return "productFilter";
   }
+
   @GetMapping("/productFilter/more")
   public String getMoreFilteredProduct(
-     HttpServletRequest request, Model model, @RequestParam int page, @RequestParam int size
+      HttpServletRequest request, Model model, @RequestParam int page, @RequestParam int size
   ) {
     String product_type = request.getParameter("product_type");
-    List<Product> moreProducts = productService.getMoreSearchFilteredProduct(product_type,page, size);
+    List<Product> moreProducts = productService.getMoreSearchFilteredProduct(product_type, page,
+        size);
     model.addAttribute("productListMore", moreProducts);
     System.out.println(product_type);
-    if(moreProducts.isEmpty())
-    {
+    if (moreProducts.isEmpty()) {
       System.out.println("empty");
     }
-    for(Product p : moreProducts)
-    {
+    for (Product p : moreProducts) {
       System.out.println(p);
     }
     return "productFilter";
@@ -181,8 +181,8 @@ public class ProductController {
     String email = String.valueOf(session.getAttribute("user_email"));
     model.addAttribute("user_email", email);
     model.addAttribute("productType", "Laptop");
-    User user = (User)session.getAttribute("user");
-    model.addAttribute("user",user);
+    User user = (User) session.getAttribute("user");
+    model.addAttribute("user", user);
     return "productFilter";
   }
 
@@ -208,63 +208,63 @@ public class ProductController {
 
     String email = String.valueOf(session.getAttribute("user_email"));
     model.addAttribute("user_email", email);
-    if(!listProduct.isEmpty())
-    {
+    if (!listProduct.isEmpty()) {
       model.addAttribute("listProduct", listProduct);
     }
 
     model.addAttribute("productType", productType);
     model.addAttribute("start_price", start_price);
     model.addAttribute("end_price", end_price);
-    User user = (User)session.getAttribute("user");
-    model.addAttribute("user",user);
+    User user = (User) session.getAttribute("user");
+    model.addAttribute("user", user);
     return "productFilter";
   }
+
   @GetMapping("/productFilter/price/more")
   public String findMoreProductByPrice(@RequestParam("start_price") int start_price,
       @RequestParam("end_price") int end_price, @RequestParam("product_type") String productType,
-      Model model,@RequestParam int page, @RequestParam int size,
-      HttpSession session)
-  {
+      Model model, @RequestParam int page, @RequestParam int size,
+      HttpSession session) {
     List<Product> productListMore = productService.getMoreProductFilterByPrice(productType,
-        start_price,end_price,page,size );
-    model.addAttribute("productListMore",productListMore);
-    model.addAttribute("product_type",productType);
+        start_price, end_price, page, size);
+    model.addAttribute("productListMore", productListMore);
+    model.addAttribute("product_type", productType);
     model.addAttribute("start_price", start_price);
     model.addAttribute("end_price", end_price);
-    User user = (User)session.getAttribute("user");
-    model.addAttribute("user",user);
+    User user = (User) session.getAttribute("user");
+    model.addAttribute("user", user);
     return "productFilter";
   }
 
   @GetMapping("/search")
   public String searchProduct(@RequestParam String keyword,
       @RequestParam int page, @RequestParam int size,
-      Model model,HttpSession session) {
+      Model model, HttpSession session) {
 
     List<Product> productList = productService.searchProduct(keyword);
     List<Product> pList = productService.getMoreSearchProduct(keyword, page, size);
     model.addAttribute("productList", productList);
     model.addAttribute("productListMore", pList);
     model.addAttribute("keyword", keyword);
-    User user = (User)session.getAttribute("user");
-    model.addAttribute("user",user);
+    User user = (User) session.getAttribute("user");
+    model.addAttribute("user", user);
     String email = String.valueOf(session.getAttribute("user_email"));
     model.addAttribute("user_email", email);
     return "homepage";
   }
+
   @GetMapping("/searchFilter")
   public String searchFilteredProduct(@RequestParam String keyword,
       @RequestParam int page, @RequestParam int size,
-      Model model,HttpSession session) {
+      Model model, HttpSession session) {
 
     List<Product> productList = productService.searchProduct(keyword);
     List<Product> pList = productService.getMoreSearchProduct(keyword, page, size);
     model.addAttribute("productList", productList);
     model.addAttribute("productListMore", pList);
     model.addAttribute("keyword", keyword);
-    User user = (User)session.getAttribute("user");
-    model.addAttribute("user",user);
+    User user = (User) session.getAttribute("user");
+    model.addAttribute("user", user);
     String email = String.valueOf(session.getAttribute("user_email"));
     model.addAttribute("user_email", email);
     return "productFilter";
